f5d1f48908842f7f411ce6cb2b4d3cae3a9fdf7c
mitubhungani <bhunganimitu@gmail.com>
2024-06-27 18:52:09 +0530

    done

--- src/BgChange.jsx
+++ src/BgChange.jsx
@@ -9,28 +9,28 @@ const BgChange = ({ bg, b, t, tt }) => {
     if (!mode) {
       document.getElementById("main").style.backgroundColor = bg;
       document.getElementById("text").style.color = tt;
+      document.getElementById("btn").innerHTML = "🔦";
     } else {
       document.getElementById("main").style.backgroundColor = b;
       document.getElementById("text").style.color = t;
+      document.getElementById("btn").innerHTML = "🌚";
     }
   };
 
   return (
     <div>
-      <div className="w-1/4 h-screen m-auto p-10">
-        <div
-          id="main"
-          className="border-2 bg-red-600 border-red-600 text-center rounded-lg p-1"
-        >
-          <h1 className="text-white" id="text">
+      <div className="h-screen w-full m-auto">
+        <div id="main" className=" w-full  h-screen  text-center p-1">
+          <h1 className="text-black" id="text">
             Hello
           </h1>
           <div className="my-3 w-full flex justify-around">
             <button
+              id="btn"
               onClick={() => change()}
-              className="text-white bg-red-800 rounded-lg px-6 py-2"
+              className="text-white bg-red-800 rounded-lg px-6 py-2 text-4xl"
             >
-              Dark
+              Mode
             </button>
             {/* <button className="text-white bg-red-800 rounded-lg px-6 py-2">Light</button> */}
           </div>
